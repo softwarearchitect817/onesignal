dead2a29adc2a0a54ceb87638514d723f424e553
Derek Jones <derekjones9817@gmail.com>
2022-07-21 20:23:19 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
